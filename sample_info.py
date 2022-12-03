d16a2a4e1a1eb71eb91b74f1185780c7745011ac
staneswilson <82250240+staneswilson@users.noreply.github.com>
2022-12-03 14:47:20 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 6351429
+API_HASH = '912c2bcd930fb7dd5bac711b8c4d4895'
+BOT_TOKEN = '5514962644:AAGLLbdUDDpHbUfdhnZEoBb8oVNEGOzy9sw'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,24 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1584694165]
+CHANNELS = [-1001689360667, -1001541175468, -1001265602872]
 AUTH_USERS = []
 AUTH_CHANNEL = None
+REQ_CHANNEL = -1001871529268
+JOIN_REQS_DB = "mongodb+srv://join:filter@cluster0.ltifpcp.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL = -1001179133650
+CUSTOM_FILE_CAPTION = """**{file_name}**({file_size}) 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+താഴെയുള്ള ലിങ്കിൽ ക്ലിക്ക് ചെയ്ത് ഗ്രൂപ്പിൽ ജോയിൻ ആയ ശേഷം സിനിമ ഡൌൺലോഡ് ചെയ്യുക. അല്ലെങ്കിൽ ഫയൽ വർക്ക്‌ ആവില്ല 😪
+🚦Group 1👉  [📽 ᴄɪɴᴇᴍᴀ ᴄᴏᴍᴘᴀɴʏ 📽](https://t.me/CinemaCompany_Group) 
+🚦Group 2👉 [🟢ᴄɪɴᴇᴍᴀ ᴛᴀʟᴋɪᴇꜱ 🟢](https://t.me/Cinema_Talkies_Group)
+🚦Group 3👉 [⭕️ᴍᴀʟʟᴜ ᴛᴀʟᴋɪᴇꜱ⭕️](https://t.me/MalluTalkies_Group)
+🚦Group 4👉 [🔅ᴍᴏᴠɪᴇ ʜᴜʙ🔅](https://t.me/MovieHub_Group)"""
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
+DATABASE_URI = "mongodb+srv://jerry:bot@cluster0.qof3hys.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'cluster0'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
